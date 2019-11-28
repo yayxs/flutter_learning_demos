c89d933696f132648eb5cdb8dad675e3ec814bc9
yangwl <yangwl@zonekey.com.cn>
2019-11-28 16:30:37 +0800

    条形统计图点击

---
+++ selection_callback_example_bar/lib/bar_sel_page.dart
@@ -0,0 +1,73 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart';
+import 'package:charts_flutter/src/text_element.dart';
+import 'package:charts_flutter/src/text_style.dart' as style;
+
+class Chart extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LineChart(
+      _createSampleData(),
+      behaviors: [
+        LinePointHighlighter(symbolRenderer: CustomCircleSymbolRenderer())
+      ],
+      selectionModels: [
+        SelectionModelConfig(changedListener: (SelectionModel model) {
+          if (model.hasDatumSelection)
+            print(model.selectedSeries[0]
+                .measureFn(model.selectedDatum[0].index));
+        })
+      ],
+    );
+  }
+
+  List<Series<LinearSales, int>> _createSampleData() {
+    final data = [
+      new LinearSales(0, 5),
+      new LinearSales(1, 25),
+      new LinearSales(2, 100),
+      new LinearSales(3, 75),
+    ];
+    return [
+      new Series<LinearSales, int>(
+        id: 'Sales',
+        colorFn: (_, __) => MaterialPalette.blue.shadeDefault,
+        domainFn: (LinearSales sales, _) => sales.year,
+        measureFn: (LinearSales sales, _) => sales.sales,
+        data: data,
+      )
+    ];
+  }
+}
+
+class CustomCircleSymbolRenderer extends CircleSymbolRenderer {
+  @override
+  void paint(ChartCanvas canvas, Rectangle<num> bounds,
+      {List<int> dashPattern,
+      Color fillColor,
+      Color strokeColor,
+      double strokeWidthPx}) {
+    super.paint(canvas, bounds,
+        dashPattern: dashPattern,
+        fillColor: fillColor,
+        strokeColor: strokeColor,
+        strokeWidthPx: strokeWidthPx);
+    canvas.drawRect(
+        Rectangle(bounds.left - 5, bounds.top - 30, bounds.width + 10,
+            bounds.height + 10),
+        fill: Color.white);
+    var textStyle = style.TextStyle();
+    textStyle.color = Color.black;
+    textStyle.fontSize = 15;
+    canvas.drawText(TextElement("1", style: textStyle), (bounds.left).round(),
+        (bounds.top - 28).round());
+  }
+}
+
+class LinearSales {
+  final int year;
+  final int sales;
+  LinearSales(this.year, this.sales);
+}
