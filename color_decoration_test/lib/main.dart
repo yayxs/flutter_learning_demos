30635dbaddf7451b6882d590432cff907c371b52
WuRong <1667629529@qq.com>
2019-12-01 01:09:47 +0800

    Container 修饰颜色问题

---
+++ color_decoration_test/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('color decoration'),
+      ),
+      body: Container(
+        width: 300,
+        height: 300,
+        // color: Colors.blueGrey,
+        decoration: BoxDecoration(),
+        child: Center(
+          child: Text('this is a text widget'),
+        ),
+      ),
+    );
+  }
+}
