2daaba5eacf8d3c5f05ffce37f1c7d3df74a6a1d
yangwl <yangwl@zonekey.com.cn>
2019-12-07 18:58:33 +0800

    画点

---
+++ scatter_plot_chart/lib/scatter_plot_chart.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+final List listData = [
+  {
+    "duration": 10.0,
+    "times": 2.0,
+    "name": "Flutter",
+    "radios"
+        "offsetX": 50.0,
+    "offsetX": 50.0
+  },
+  {"duration": 3, "times": 1, "name": "Dart", "flag": 1},
+];
+
+class CustomPaintRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CustomPaint(
+        size: Size(300, 300), //指定画布大小
+        painter: MyPainter(), // 背景画笔，会显示在子节点后面
+      ),
+    );
+  }
+}
+
+class MyBackPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..isAntiAlias = true
+      ..style = PaintingStyle.fill //填充
+      ..color = Colors.grey[100]; //背景为纸黄色
+    canvas.drawRect(Offset.zero & size, paint);
+
+    paint
+      ..style = PaintingStyle.stroke //线
+      ..color = Colors.white
+      ..strokeWidth = 1.0;
+  }
+
+  //在实际场景中正确利用此回调可以避免重绘开销
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
+
+class MyPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+
+    _myDrawCircle(double offsetX, double offsetY, double radius, Paint paint) {
+      canvas.drawCircle(
+        Offset(offsetX, offsetY),
+        radius,
+        paint,
+      );
+    }
+
+    for (int i = 0; i <= listData.length; ++i) {
+      paint
+        ..style = PaintingStyle.fill
+        ..color = Colors.lightBlue;
+      if (i == 0) {
+        _myDrawCircle(50.0, 50.0, 10.0, paint);
+      } else {
+        _myDrawCircle(50.0 + 50 * i, 50, 10.0, paint);
+      }
+    }
+  }
+
+  //在实际场景中正确利用此回调可以避免重绘开销
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
