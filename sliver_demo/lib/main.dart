363caa4aaac2bd2520c74f32c75584de501f63e2
yangwl <yangwl@zonekey.com.cn>
2019-12-12 20:37:08 +0800

    完善README

--- sliver_demo/lib/main.dart
+++ sliver_demo/lib/main.dart
@@ -11,7 +11,9 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: CustomScrollViewPage());
+        home: Scaffold(
+          body: CustomScrollViewPage(),
+        ));
   }
 }
 
